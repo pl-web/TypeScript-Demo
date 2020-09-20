935e9e09a4b793f72d71cd275be6e8ec2079fa73
it-reader <hnature@126.com>
2020-09-21 00:11:24 +0800

    typescript-basic-type

---
+++ src/index.ts
@@ -0,0 +1,1 @@
+import './typescript/basic-type.ts'
---
+++ src/typescript/basic-type.ts
@@ -0,0 +1,21 @@
+// Boolean
+let bool: boolean = true
+console.log("boolean type:", bool)
+// number
+let num: number
+num = 0
+console.log("number type: ", num)
+// String
+let str: string
+str = 'abcdefg'
+console.log("string type: ", str)
+// Array
+let arr: number[]
+arr = [1,2,3]
+console.log("number array arr: ", arr)
+let arr_ : Array<number>
+arr_ = [1,2,3,4,5]
+console.log("number array arr_: ", arr_)
+let _arr: (string | number)[]
+_arr = [1,2,3,'a','b']
+console.log("string number array _arr: ", _arr)
