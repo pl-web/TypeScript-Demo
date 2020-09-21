b2ad202a4c6f8cecb3ce1eaa51b99d222213993c
it-reader <hnature@126.com>
2020-09-21 22:16:00 +0800

    enum枚举类型的数据

--- src/typescript/basic-type.ts
+++ src/typescript/basic-type.ts
@@ -19,3 +19,18 @@ console.log("number array arr_: ", arr_)
 let _arr: (string | number)[]
 _arr = [1,2,3,'a','b']
 console.log("string number array _arr: ", _arr)
+// Tuble
+let tuble: [string, number, boolean]
+tuble = ['1', 2, true]
+// 枚举
+enum animal {
+  Dog,
+  Cat,
+  Mouse
+}
+console.log(animal.Dog) // 0
+console.log(animal.Cat) // 1
+console.log(animal.Mouse) // 2
+console.log(animal[0]) // Dog
+console.log(animal[1]) // Cat
+console.log(animal[2]) // Mouse
