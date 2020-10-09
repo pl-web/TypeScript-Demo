75c9d234cc95ba9d2ea8c76169b1b8101e5aad1f
it-reader <hnature@126.com>
2020-10-09 23:28:50 +0800

    class

--- src/typescript/class.js
+++ src/typescript/class.js
@@ -1,1 +1,26 @@
 // 类
+function User(name, age) {
+  this.name = name;
+  this.age = age;
+}
+
+User.prototype.toIntroduction =  function() {
+  console.log('我的名字是' + this.name + ", 今年" + this.age + "岁了")
+}
+
+const u1 = new User('pp', 20)
+
+console.log("User - 01: u1 ", u1)
+
+u1.toIntroduction()
+
+// --------------------------------------------------------------------
+class Classmate{
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  answerQuestion() {
+    console.log("我是"+this.name+", 我要回答问题")
+  }
+}
