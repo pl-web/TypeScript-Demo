56712ba29a82c73ba412ed3b8ac0e739ec5c7174
it-reader <hnature@126.com>
2020-10-09 22:38:04 +0800

    Ts泛型

--- src/typescript/generics.ts
+++ src/typescript/generics.ts
@@ -1,5 +1,16 @@
-const createArray = (value: any, times: number = 5) => {
+const createArray = <T>(value: T, times: number = 5): T[] => { 
   return new Array(times).fill(value)
 }
 
-console.log(createArray(2))
+console.log(createArray<number>(2))
+console.log()
+
+const createObject = () => {
+
+}
+
+const createTuble = <T, U>(params1: T, params2: U, times: number): Array<[T, U]> => {
+  return new Array(times).fill([params1, params2])
+}
+const t = createTuble(1, 'abc', 3)
+console.log(t)
