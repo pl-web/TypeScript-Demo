ef762aea3234fb1cf1b4871723cb04bd6f7afc44
it-reader <hnature@126.com>
2020-10-09 23:20:19 +0800

    Ts泛型

--- src/typescript/generics.ts
+++ src/typescript/generics.ts
@@ -14,3 +14,11 @@ const createTuble = <T, U>(params1: T, params2: U, times: number): Array<[T, U]>
 }
 const t = createTuble(1, 'abc', 3)
 console.log(t)
+
+t.forEach((item) => {
+  console.log("t", item)
+  console.log(item[0])
+  console.log(item[1])
+})
+
+// const getOneArray = <T>(argument01: T, times: number) => T[]
