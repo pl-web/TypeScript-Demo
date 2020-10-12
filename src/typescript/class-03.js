851804730d1eda6d1d3fb7405348df3ee2adbc16
it-reader <hnature@126.com>
2020-10-12 23:38:02 +0800

    extends-class

--- src/typescript/class-03.js
+++ src/typescript/class-03.js
@@ -11,3 +11,25 @@ function Time() {
 Time.prototype = new Food()
 const t = new Time()
 console.log(t)
+
+// ------------------------------------------
+console.log("-------------------------------")
+class P {
+  constructor(surname) {
+    this.surname = surname
+  }
+  getSurname() {
+    return this.surname
+  }
+}
+class C extends P {
+  constructor(surname, name, address ,age) {
+    super(surname)
+    this.name = name
+    this.address = address
+    this.age = age
+  }
+}
+const c = new C('i', 'ining', 'solar system', 1)
+console.log(c)
+console.log(c.getSurname())
